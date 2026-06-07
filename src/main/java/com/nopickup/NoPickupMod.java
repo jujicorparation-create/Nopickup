f8f678eabd362b58ad67c0780a858f74f6129849
jujicorparation-create <jujicorparation@gmail.com>
2026-06-07 16:34:17 +0500

    Update NoPickupMod.java

--- src/main/java/com/nopickup/NoPickupMod.java
+++ src/main/java/com/nopickup/NoPickupMod.java
@@ -1,13 +1,8 @@
 package com.nopickup;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.entity.event.v1.ServerEntityCombatEvents;
-import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.entity.ItemEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 
 public class NoPickupMod implements ClientModInitializer {
 
@@ -25,19 +20,6 @@ public class NoPickupMod implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client.player == null || client.world == null) return;
-            
-            client.world.getEntities().forEach(entity -> {
-                if (entity instanceof ItemEntity itemEntity) {
-                    ItemStack stack = itemEntity.getStack();
-                    if (shouldBlock(stack)) {
-                        // Item pickup range dan chiqarib yuboramiz
-                        itemEntity.setPickupDelay(32767);
-                    }
-                }
-            });
-        });
     }
 
     public static boolean shouldBlock(ItemStack stack) {
@@ -48,4 +30,4 @@ public class NoPickupMod implements ClientModInitializer {
         }
         return false;
     }
-    }
+             }
