d6b77a244563c7be0adc66c95b8af507ee9ad942
jujicorparation-create <jujicorparation@gmail.com>
2026-06-07 12:56:30 +0500

    Update NoPickupMixin.java

--- src/main/java/com/nopickup/mixin/NoPickupMixin.java
+++ src/main/java/com/nopickup/mixin/NoPickupMixin.java
@@ -7,15 +7,16 @@ import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PlayerEntity.class)
-public class NoPickupMixin {
+@Mixin(ItemEntity.class)
+public abstract class NoPickupMixin {
 
-    @Inject(method = "canPickupItem", at = @At("HEAD"), cancellable = true)
-    private void onCanPickup(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "onPlayerCollision", at = @At("HEAD"), cancellable = true)
+    private void onPickup(PlayerEntity player, CallbackInfo ci) {
+        ItemStack stack = ((ItemEntity)(Object)this).getStack();
         if (NoPickupMod.shouldBlock(stack)) {
-            cir.setReturnValue(false);
+            ci.cancel();
         }
     }
-        }
+            }
