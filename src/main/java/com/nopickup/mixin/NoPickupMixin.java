015697360e667782010c8469c856bef999c69e40
jujicorparation-create <jujicorparation@gmail.com>
2026-06-10 00:42:11 +0500

    Update NoPickupMixin.java

--- src/main/java/com/nopickup/mixin/NoPickupMixin.java
+++ src/main/java/com/nopickup/mixin/NoPickupMixin.java
@@ -1,22 +1,26 @@
 package com.nopickup.mixin;
 
-import com.nopickup.NoPickupMod;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import com.nopickup.NoPickupMod;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PlayerEntity.class)
-public abstract class NoPickupMixin {
+@Mixin(ItemEntity.class)
+public class NoPickupMixin {
 
-    @Inject(method = "triggerItemPickedUpByEntityCriteria", at = @At("HEAD"), cancellable = true)
-    private void onPickup(ItemEntity itemEntity, CallbackInfo ci) {
+    @Inject(method = "onPlayerCollision", at = @At("HEAD"), cancellable = true)
+    private void onPickup(PlayerEntity player, CallbackInfo ci) {
+        // 'this' orqali ItemEntity obyektini olamiz
+        ItemEntity itemEntity = (ItemEntity) (Object) this;
         ItemStack stack = itemEntity.getStack();
+
+        // Agar siz yaratgan NoPickupMod-da bloklash funksiyasi aktiv bo'lsa va element ro'yxatda bo'lsa
         if (NoPickupMod.shouldBlock(stack)) {
-            ci.cancel();
+            ci.cancel(); // Player narsaga tegsa ham uni inventariga ololmaydi
         }
     }
 }
